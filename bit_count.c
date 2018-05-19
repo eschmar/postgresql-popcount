074742d90e5e6f8386345aa777ff63acc54da9bb
Marcel Eschmann <marcel.e@nectima.se>
2018-05-19 15:31:09 +0200

    Implement 64bit hamming weight version

--- bit_count.c
+++ bit_count.c
@@ -20,6 +20,11 @@ static const int bitcount[256]={
     3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8
 };
 
+static const uint64_t m1  = 0x5555555555555555; // 0b0101...
+static const uint64_t m2  = 0x3333333333333333; // 0b00110011...
+static const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; // 0b00001111...
+static const uint64_t h01 = 0x0101010101010101; // the sum of 256 to the power of 0, 1, 2, 3...
+
 static int hamming_weight_32bit(int val) {
     // if (val == 0) return 0;
     val = val - ((val >> 1) & 0x55555555);
@@ -29,7 +34,14 @@ static int hamming_weight_32bit(int val) {
     return val;
 }
 
-// mask = (1 << param) - 1;
+static int hamming_weight_64bit(uint64_t val)
+{
+    // if (val == 0) return 0;
+    val -= (val >> 1) & m1;
+    val = (val & m2) + ((val >> 2) & m2);
+    val = (val + (val >> 4)) & m4;
+    return (val * h01) >> 56;
+}
 
 Datum
 bit_count(PG_FUNCTION_ARGS) {
@@ -85,9 +97,37 @@ bit_count_32bit(PG_FUNCTION_ARGS) {
 
 Datum
 bit_count_64bit(PG_FUNCTION_ARGS) {
-    // VarBit *a = PG_GETARG_VARBIT_P(0);
+    VarBit *a = PG_GETARG_VARBIT_P(0);
 
     int count = 0;
-    // todo.
+    int length = VARBITLEN(a);
+    unsigned char *byte_pointer = VARBITS(a);
+    unsigned long *position = (unsigned long *) byte_pointer;
+
+    while (length >= 64) {
+        count += hamming_weight_64bit(*position);
+        length -= 64;
+        position++;
+    }
+
+    if (length == 0) PG_RETURN_INT32(count);
+
+    // special case, non-64bit-aligned varbit length
+    byte_pointer = (unsigned char *) position;
+    unsigned char val;
+    long remainder = 0;
+    int i;
+
+    for (i = (length / 8); i > 0; i--) {
+        val = *byte_pointer;
+        remainder += ((long) val) << (i * 8);
+        byte_pointer++;
+    }
+
+    // last byte may not be aligned
+    val = *byte_pointer;
+    remainder += (long) (val >> (8 - (length % 8)));
+
+    count += hamming_weight_64bit(remainder);
     PG_RETURN_INT32(count);
 }
