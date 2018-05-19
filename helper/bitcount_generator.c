23c180d44f5e5ab54adf7054d1e09fde15f00ebd
Marcel Eschmann <marcel.e@nectima.se>
2018-05-19 17:14:21 +0200

    Add lookup table generator

---
+++ helper/bitcount_generator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int n = 256;
+
+/**
+ * Hamming weight algorithm using 12 arithmetic operations.
+ * 32bit version of the algorithm.
+ **/
+static int hamming_weight_32bit(int val) {
+    val = val - ((val >> 1) & 0x55555555);
+    val = (val & 0x33333333) + ((val >> 2) & 0x33333333);
+    val = (val + (val >> 4)) & 0x0f0f0f0f;
+    val = (val * 0x01010101) >> 24;
+    return val;
+}
+
+void init(int argc, char *argv[]) {
+    int flag;
+
+    while ((flag = getopt (argc, argv, "n:")) != -1) {
+        switch (flag) {
+            case 'n':
+                n = atoi(optarg);
+                break;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    init(argc, argv);
+    int current = -1;
+
+    while (++current < n) {
+        printf("%d, ", hamming_weight_32bit(current));
+    }
+
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -5,3 +5,15 @@ Requires `pg_config` installed.
 make install
 make installcheck
 ```
+
+## generate lookup table
+```sh
+cd helper
+gcc -o bitcount_generator.o bitcount_generator.c
+./bitcount_generator.o -n 256
+```
+
+## todo
+* [ ] Move hamming weight to lib
+* [ ] Avoid redundant code 64bit variant
+* [ ] Bump v1
