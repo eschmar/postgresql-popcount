c06328414393b1faa957ce4a15ac04f50ba6503e
Marcel Eschmann <marcel.e@nectima.se>
2018-05-20 22:34:41 +0200

    Finish mysql benchmark

--- helper/mysql.sh
+++ helper/mysql.sh
@@ -4,28 +4,53 @@ CYAN='\033[0;36m'
 MAGENTA='\033[0;35m'
 NC='\033[0m'
 
-table="bit_count"
-size=100
+trials=50
+base="bit_count"
+database="bit_count"
+drop='true'
 
-while getopts 'n:t:' flag; do
+while getopts 't:d:x' flag; do
     case "${flag}" in
-        n) size=$OPTARG ;;
-        t) table=$OPTARG ;;
+        t) trials=$OPTARG ;;
+        d) database=$OPTARG ;;
+        x) drop='false' ;;
         *) error "Unexpected option ${flag}" ;;
     esac
 done
 
-table="${table}_${size}"
-
-echo "
-CREATE TABLE \`$table\` (
-    \`id\` int(11) unsigned NOT NULL AUTO_INCREMENT,
-    \`bit\` bit(64) DEFAULT NULL,
-    PRIMARY KEY (\`id\`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-"
-
-for (( i=1; i<=$size; i++ ))
+for samples in 100 500 1000 2500 5000 7500 10000 25000 50000 75000 100000 250000 500000 750000 1000000
 do
-    printf "INSERT INTO $table VALUES (NULL, $i);\n"
+    printf "$samples "
+
+    table="${base}_${samples}"
+
+    mysql -u root "$database" -e "DROP TABLE \`$table\`;"
+    mysql -u root "$database" -e "CREATE TABLE \`$table\` (\`id\` int(11) unsigned NOT NULL AUTO_INCREMENT, \`bit\` bit(64) DEFAULT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB AUTO_INCREMENT=1000001 DEFAULT CHARSET=utf8;"
+    mysql -u root "$database" -e "TRUNCATE TABLE \`$table\`;"
+
+    for (( i=1; i<=$samples; i++ ))
+    do
+        mysql -u root "$database" -e "INSERT INTO $table VALUES (NULL, $i);"
+    done
+
+    query="SET profiling = 1; SELECT SUM(count) FROM (SELECT BIT_COUNT(bit) as count FROM $table WHERE 1) as bits; SHOW PROFILES;"
+
+    for (( i=1; i<=$trials; i++ ))
+    do
+        temp=$(mysql -u root -vvv $database -e "$query" | grep "| SELECT SUM(count)" | grep -Eo '[0-9][.][0-9]+')
+
+        printf "$temp"
+        if [ $i -lt $trials ]
+        then
+            printf " "
+        fi
+    done
+
+    case $drop in
+        'true')
+            mysql -u root $database -e "DROP TABLE \`$table\`;"
+            ;;
+    esac
+
+    echo
 done
