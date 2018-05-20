de6257c1e19a77bf841e472d6e4782fcc928a790
Marcel Eschmann <marcel.e@nectima.se>
2018-05-20 15:35:35 +0200

    Make colored output optional

--- helper/experiment.sh
+++ helper/experiment.sh
@@ -8,24 +8,42 @@ strategy='bit_count'
 trials=10
 bitlength=512
 units='false'
+color='false'
 
-while getopts 's:t:l:u' flag; do
+while getopts 's:t:l:uc' flag; do
     case "${flag}" in
         s) strategy=$OPTARG ;;
         t) trials=$OPTARG ;;
         l) bitlength=$OPTARG ;;
         u) units='true' ;;
+        c) color='true' ;;
         *) error "Unexpected option ${flag}" ;;
     esac
 done
 
-printf "> Strategy: ${MAGENTA}$strategy${NC}\n"
-printf "> Bit length: ${MAGENTA}$bitlength${NC}\n"
-printf "> Trials: ${MAGENTA}$trials${NC}\n\n"
+case $color in
+    'true')
+        printf "> Strategy: ${MAGENTA}$strategy${NC}\n"
+        printf "> Bit length: ${MAGENTA}$bitlength${NC}\n"
+        printf "> Trials: ${MAGENTA}$trials${NC}\n\n"
+        ;;
+    'false')
+        printf "> Strategy: $strategy\n"
+        printf "> Bit length: $bitlength\n"
+        printf "> Trials: $trials\n\n"
+        ;;
+esac
 
 for samples in 100 500 1000 2500 5000 7500 10000 25000 50000 75000 100000 250000 500000 750000 1000000
 do
-    printf "${CYAN}$samples${NC} "
+    case $color in
+        'true')
+            printf "${CYAN}$samples${NC} "
+            ;;
+        'false')
+            printf "$samples "
+            ;;
+    esac
 
     query="SELECT sum($strategy(x::bit($bitlength))) FROM generate_series(1,$samples) x;"
     count=0
