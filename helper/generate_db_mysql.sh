27cd118800effd9192a9cd41f357a5a14f24eacd
Marcel Eschmann <marcel.e@nectima.se>
2018-05-22 20:47:23 +0200

    Introduce more efficient mysql db generator

---
+++ helper/generate_db_mysql.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+MAGENTA='\033[0;35m'
+NC='\033[0m'
+
+base="bit_count"
+database="bit_count"
+
+while getopts 'd:' flag; do
+    case "${flag}" in
+        d) database=$OPTARG ;;
+        *) error "Unexpected option ${flag}" ;;
+    esac
+done
+
+for samples in 100 500 1000 2500 5000 7500 10000 25000 50000 75000 100000 250000 500000 750000 1000000
+do
+    printf "$samples "
+    table="${base}_$(printf %07d $samples)"
+
+    mysql -u root "$database" -e "DROP TABLE IF EXISTS \`$table\`;"
+    mysql -u root "$database" -e "CREATE TABLE \`$table\` (\`id\` int(11) unsigned NOT NULL AUTO_INCREMENT, \`bit\` bit(64) DEFAULT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB AUTO_INCREMENT=1000001 DEFAULT CHARSET=utf8;"
+    mysql -u root "$database" -e "TRUNCATE TABLE \`$table\`;"
+
+    values=''
+    for (( j=0; j<($samples/100); j++))
+    do
+        values=''
+        for (( i=1; i<=100; i++ ))
+        do
+            values="$values(NULL, $((j*100+i)))"
+            if [[ $i -ne 100 ]]; then values="$values," ; fi
+        done
+
+        # insert here
+        mysql -u root "$database" -e "INSERT INTO $table VALUES $values;"
+    done
+done
