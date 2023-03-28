4e4556772b700970925653e779bcface84a52935
Marcel Eschmann <marcel.eschmann@gmail.com>
2023-03-28 16:04:23 +0200

    Improve database generation with randomised bit strings

--- helper/generate_db_postgres.sh
+++ helper/generate_db_postgres.sh
@@ -6,41 +6,39 @@ NC='\033[0m'
 
 base="bit_count"
 database="bit_count"
-alignment=64
-samples="100 500 1000 2500 5000 7500 10000 25000 50000 75000 100000 250000 500000 750000 1000000"
+
+domain=500000
+alignments="2048 4096 8192 15040 16384 32768 65536 131072"
 
 while getopts 'd:a:s:' flag; do
     case "${flag}" in
-        d) database=$OPTARG ;;
-        a) alignment=$OPTARG ;;
+        d) domain=$OPTARG ;;
+        a) alignments=$OPTARG ;;
         s) samples=$OPTARG ;;
         *) error "Unexpected option ${flag}" ;;
     esac
 done
 
-read -a arr <<< "$samples"
+read -a arr <<< "$alignments"
 
-for sample in "${arr[@]}"
+for alignment in "${arr[@]}"
 do
-    printf "$sample "
-    table="${base}_${alignment}_$(printf %07d $sample)"
+    table="${base}_${alignment}_$(printf %07d $domain)"
+    printf "> Generating table: ${MAGENTA}$table${NC}\n"
 
     # psql -U postgres -d database_name -c 
     psql -q -c "DROP TABLE IF EXISTS \"$table\";"
     psql -q -c "CREATE TABLE \"$table\" (\"id\" SERIAL, \"bit\" bit($alignment), PRIMARY KEY (\"id\"));"
     psql -q -c "TRUNCATE TABLE \"$table\";"
 
-    values=''
-    for (( j=0; j<($sample/100); j++))
-    do
-        values=''
-        for (( i=1; i<=100; i++ ))
-        do
-            values="$values($((j*100+i))::bit($alignment))"
-            if [[ $i -ne 100 ]]; then values="$values," ; fi
-        done
+    hexLength=$(($alignment/8))
 
-        # insert here
-        psql -q -c "INSERT INTO $table (bit) VALUES $values;"
+    for (( j=0; j<($domain); j++))
+    do
+        echo -ne ">>> Adding rows ($j/$domain).\r"
+        randomhex=$(openssl rand -hex $hexLength)
+        psql -q -c "INSERT INTO $table (bit) VALUES (x'$randomhex'::bit($alignment));"
     done
+
+    echo -ne "\n"
 done
